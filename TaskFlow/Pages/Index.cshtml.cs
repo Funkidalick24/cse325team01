7ec508a8d91f1bc66233ed174f6939adfcbd72b0
simeonchirunga <schirunga1@byupathway.edu>
2026-03-25 15:45:07 +0200

    Initial setup: ASP.NET Core project with Identity, models, migrations, and basic task CRUD

---
+++ TaskFlow/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TaskFlow.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
